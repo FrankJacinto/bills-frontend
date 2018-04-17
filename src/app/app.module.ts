5785d7de9bfe83fecc4552de738cd42e160924b4
MarioMario <marioramos083@gmail.com>
2018-04-17 11:14:34 -0500

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './componentescomunes/login/login.component';
-import {AppRouterModule}  from './app-routing-module'
+import { AppRouterModule }  from './app-routing-module';
+import { HttpModule } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import {AppConfig} from './servicioscomunes/app-config';
 import {ApiRequestService} from './servicioscomunes/api-request.service';
 import {AuthService} from './servicioscomunes/auth.service';
 import {AuthGuardService} from './servicioscomunes/auth-guard.service';
+import {LoginService} from './componentescomunes/login/login.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent
   ],
+  entryComponents: [
+    LoginComponent
+  ],
   imports: [
     BrowserModule,
-    AppRouterModule
+    AppRouterModule,
+    FormsModule,
+    HttpModule,
+    ReactiveFormsModule,
+    NgbModule.forRoot(),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [
     AppConfig,
     ApiRequestService,
     AuthService,
-    AuthGuardService
+    AuthGuardService,
+    NgbActiveModal,
+    LoginService,
+    HttpClient
   ],
   bootstrap: [AppComponent]
 })
