ee0bb1e1ee213bbea9f7330dd5382526b3b71ade
Christhian <christhian2524@gmail.com>
2018-04-18 06:03:24 -0500

    primeNG

--- src/app/modulos/empresa/empresa.module.ts
+++ src/app/modulos/empresa/empresa.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import {EmpresaRoutingModule} from './empresa-routing-module'
 import { ConfiguracionComponent } from './configuracion/configuracion.component';
 import { EmpresaComponenteComponent } from './empresa-componente/empresa-componente.component';
 import {UsuariosComponent} from '../../componentescomunes/usuarios/usuarios.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//primeNG
+import {TableModule} from 'primeng/table';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   imports: [
     CommonModule,
     EmpresaRoutingModule,
+    TableModule,
+    FormsModule,
+    ButtonModule,
+    ReactiveFormsModule
   ],
   declarations: [
     ConfiguracionComponent,
