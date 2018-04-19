a6bffeee51b0ddefefc86d36c016bae2573bbbd0
luisor <luisorstaby@gmail.com>
2018-04-19 10:45:44 -0500

    perfilvista

---
+++ src/app/entidades/entidad.regimen.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by LUIS ORTIZ on 18/04/2018.
+ */
+export class Regimen{
+    id:number;
+    descripcion:string;
+    estado:boolean;
+}
---
+++ src/app/entidades/entidad.ubigeo.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by LUIS ORTIZ on 18/04/2018.
+ */
+import {Tipoubigeo} from "./entidad.tipoubigeo";
+
+export class Ubigeo{
+    id:number;
+    codigo:string;
+    estado:boolean;
+    nombre:string;
+    idtipoubigeo:Tipoubigeo;
+}
